2223cc018ee978b555d50e9a7d69fbacaafb69c3
nicway <nicway@u.washington.edu>
2015-06-01 16:18:31 -0700

    Adding sub functions and new rules

--- Master_QC_script.m
+++ Master_QC_script.m
@@ -3,21 +3,23 @@ clear all; close all; clc;
 %% Script to import and QC noisy acoustic snow depth data
  
 % Data to QC
-% New Snow is in column 17. 
 filein   = 'G:\ncar\d1\data\Point_Snow_Sites\Raw\SNQ_Hist\QC_Data.mat';
 load(filein)
 
 % Deal with time step issue
-SD_24_acou = QC_Data(:,17);
 TIME_in    = QC_Data(:,1);
-dt_all = diff(TIME_in)*24; % hours
+% dt_all = diff(TIME_in)*24; % hours
 
-[QC_Data_out, TIME_out] = Import_intermittent_time_data_to_continuous(TIME_in,QC_data);
+[QC_Data_out, TIME_out] = Import_intermittent_time_data_to_continuous(TIME_in,QC_Data,1);
+clear QC_Data TIME_in
+
+% New Snow is in column 17. 
+SD_24_acou = in_to_m(QC_Data_out(:,17));
 
 
 % Manual observations used to aid QC'ing of automatic
 filein_B = 'G:\Work\e\Data\Obs\Intermitent_Snow_Forcing_Sites\SNQ\NWAC\NWAC data\Snoq_Wx_1973-2014 (NWAC edited by Eric).xlsx';
-datain = importdata(filein);
+datain = importdata(filein_B);
 TIME = datenum(datain.textdata(2:end,1));
 TIME_all = time_builder(TIME);
 Data = datain.data;
@@ -28,10 +30,44 @@ I_2000 = find(TIME_all(:,1)==1900);
 I_2001 = find(TIME_all(:,1)==1901);
 TIME_all(I_2000,1) = 2000;
 TIME_all(I_2001,1) = 2001;
-TIME_dly_man = datenum(TIME_all(:,1),TIME_all(:,2),TIME_all(:,3),TIME_all(:,4),TIME_all(:,5),0);
+hour_force = 6;
+TIME_dly_man = datenum(TIME_all(:,1),TIME_all(:,2),TIME_all(:,3),hour_force,TIME_all(:,5),0);
 TIME_dly_man_all = time_builder(TIME_dly_man);
 clear TIME TIME_all
 
-SD_24  = cm_to_m(Data(:,7)); % cm to meters
+SD_24_man  = cm_to_m(Data(:,7)); 
+
+% Plot raw data
+figure; hold on
+plot(TIME_out(:,7),SD_24_acou,'k')
+plot(TIME_dly_man,SD_24_man,'b*')
+legend('Acoustic raw','manual obs')
+tlabel
+
+% QC rules
+
+% 1) Acoustic data at time of manual sample should be within insturment +
+% manual accuracy
+
+% 2) Rate of change for hourly snow depth should be less than limit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
